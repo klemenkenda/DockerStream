39108760d2cf7cf1ddfec8b9dafe537a511a2390
Klemen Kenda <klemen.kenda@ijs.si>
2017-11-07 00:12:02 +0200

    Adding kafka-manager.

--- start-kafka.sh
+++ start-kafka.sh
@@ -1,2 +1,2 @@
-sudo docker run -d -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=`docker-machine ip \`docker-machine active\`` --env ADVERTISED_PORT=9092 --name kafka spotify/kafka
-sudo docker run -it --name kafkaClient spotify/kafka bash
+docker run -d -p 2181:2181 -p 9092:9092 --env ADVERTISED_HOST=`docker-machine ip \`docker-machine active\`` --env ADVERTISED_PORT=9092 --name kafka spotify/kafka
+docker run -it --name kafkaClient spotify/kafka bash
